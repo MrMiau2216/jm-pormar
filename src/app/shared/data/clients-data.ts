a99e2d26de5ad5ef92be52a5f73f976fc9bdbff0
Josef Antoni Meza Marlo <josef_meza-lol@hotmail.com>
2026-07-15 15:13:12 -0500

    Actualiza frontend e integra imágenes JM Pormar

--- src/app/shared/data/clients-data.ts
+++ src/app/shared/data/clients-data.ts
@@ -1,33 +1,28 @@
 export interface ClientItem {
-  id: number;
   name: string;
-  logo?: string;
+  logo: string;
   active: boolean;
 }
 
 export const CLIENTS_DATA: ClientItem[] = [
   {
-    id: 1,
     name: 'Municipalidad de Miraflores',
-    logo: '/logos/municipalidad-miraflores.png',
+    logo: '/logos/clientes/municipalidad-miraflores.png',
     active: true
   },
   {
-    id: 2,
     name: 'Municipalidad de Chorrillos',
-    logo: '/logos/municipalidad-chorrillos.png',
+    logo: '/logos/clientes/municipalidad-chorrillos.png',
     active: true
   },
   {
-    id: 3,
     name: 'Parque del Recuerdo',
-    logo: '/logos/parque-del-recuerdo.png',
+    logo: '/logos/clientes/parque-recuerdo.png',
     active: true
   },
   {
-    id: 4,
     name: 'DIRESA Callao',
-    logo: '/logos/diresa-callao.png',
+    logo: '/logos/clientes/diresa-callao.png',
     active: true
   }
 ];
